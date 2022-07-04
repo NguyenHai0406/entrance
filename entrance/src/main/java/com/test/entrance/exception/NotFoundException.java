1ccd36d06af762a6d7867debba602e5544ba6dd4
NguyenHai0406 <phuocson1781@gmail.com>
2022-07-04 17:03:43 +0700

    Delete entrance directory

--- entrance/src/main/java/com/test/entrance/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.test.entrance.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String code) {
-        super(code);
-    }
-}
--- entrance/src/main/java/com/test/entrance/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.test.entrance.exception;
-
-public class ValidationException  extends RuntimeException {
-    public ValidationException(String code) {
-        super(code);
-    }
-}
